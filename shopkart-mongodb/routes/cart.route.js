ea52507720d0ca9e70890e89a38a446333e6edba
abhishek kandwal <34614893+abhishek-kandwal@users.noreply.github.com>
2020-03-24 15:04:28 +0700

    Update cart.route.js

--- shopkart-mongodb/routes/cart.route.js
+++ shopkart-mongodb/routes/cart.route.js
@@ -7,9 +7,9 @@ route.get('/', cartController.empty);
 
 route.post('/remove/:product_id', cartController.removeitem);
 
-// route.post('/addmore/:product_id', cartController.addmore);
+route.post('/addmore/:product_id', cartController.addmore);
 
-// route.post('/submore/:product_id', cartController.submore);
+route.post('/submore/:product_id', cartController.submore);
 
 route.post('/:product_id', cartController.additem);
 
